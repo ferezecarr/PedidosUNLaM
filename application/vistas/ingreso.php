2cfa66a79e048cfb92248446e93ea7f6735cd946
ferezecarr <ferezecarr@gmail.com>
2018-10-02 12:26:51 -0300

    Agregando nuevas vistas

--- application/vistas/ingreso.php
+++ application/vistas/ingreso.php
@@ -29,37 +29,28 @@
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a href="index.php">Home</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Registro</li>
+                    <li class="breadcrumb-item active" aria-current="page">Ingreso</li>
                 </ol>
             </nav>
 
-            <div class="card">
+            <div class="card" style="">
                 <div class="card-header">
                     Ingresar a mi cuenta
                 </div>
                 <div class="card-body" style="">
                     <form action="" method="post">
                         <div class="form-group">
-                            <label for="nombre">Nombre:</label>
-                            <input type="text" name="nombre" id="nombre" class="form-control" placeholder="Escriba su nombre">
-                        </div>
-                        <div class="form-group">
-                            <label for="apellido">Apellido:</label>
-                            <input type="text" name="apellido" id="apellido" class="form-control" placeholder="Escriba su apellido">
-                        </div>
-                        <div class="form-group">
-                            <label for="email">E-mail:</label>
+                            <label for="">E-mail:</label>
                             <input type="email" name="email" id="email" class="form-control" placeholder="Escriba su email">
                         </div>
                         <div class="form-group">
-                            <label for="password">Password:</label>
+                            <label for="">Password:</label>
                             <input type="password" name="password" id="password" class="form-control" placeholder="Escriba su password">
                         </div>
+                        <div class="form-group">
+                            <input type="checkbox" name="recordarme" id="recordarme"> Recordarme
+                        </div>
                         <button type="submit" class="btn btn-success btn-lg btn-block">Ingresar</button>
-                        <br>
-                        <p class="text-center">
-                            ¿Ya tenes una cuenta? <a href="ingreso.php">Ingrese Aqui</a>
-                        </p>
                     </form>
                 </div>
             </div>
