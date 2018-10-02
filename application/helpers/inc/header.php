ba792c8927a86bfe56b4f817dbcf4152c46f6d4e
ferezecarr <ferezecarr@gmail.com>
2018-10-02 12:38:54 -0300

    Agregando cosas en el index

--- application/helpers/inc/header.php
+++ application/helpers/inc/header.php
@@ -3,6 +3,7 @@
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>Delivery de Comida Online - Elegí , Pedí y ya | Pedidos UNLaM</title>
 <link rel="stylesheet" href="bootstrap-4.1.3/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="css/estilos.css">
 <link rel="icon" href="../vistas/img/pedidos-unlam.png">
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 <script src="bootstrap-4.1.3/dist/js/bootstrap.min.js"></script>
