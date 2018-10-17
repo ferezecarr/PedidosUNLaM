2f264298988471761855ba2d1bf42ec36eb02d6b
ferezecarr <ferezecarr@gmail.com>
2018-10-17 10:02:21 -0300

    Login

--- application/helpers/seguridad/ValidarLogin.php
+++ application/helpers/seguridad/ValidarLogin.php
@@ -20,8 +20,8 @@ $statement = $conexion->prepare($query);
 $statement->bind_param('ss',$email,$password);
 $statement->execute();
 $resultado = $statement->get_result();
-$row = mysql_affected_rows($resultado);
-
+$row = mysqli_num_rows($resultado);
+//$row = mysql_affected_rows($resultado);
 
 
 /*if(isset($_COOKIE['recordarme2'])){
@@ -31,25 +31,22 @@ $row = mysql_affected_rows($resultado);
     setcookie('recordarme2',$recordarme,0,'/');
 }*/
 
+/*echo $resultado->num_rows;
+exit;*/ 
+
 if($resultado->num_rows > 0){
 
-    $id = $row[6];
-    $rol = $row[2];
-    $email = $row[3];
-    $tipoRol = $row[4];
-    $nombre = $row[5];
-    $telefono = $row[7];
-    $direccion = $row[8];
-    $edad = $row[9];
+   
+    
+    $id = $row;
+    $rol = $row[];
+    echo var_dump($rol);
+    exit();
+    $nombre = $row;
 
     $_SESSION['id'] = $id;
-	$_SESSION['email'] = $email;
 	$_SESSION['rol'] = $rol;
 	$_SESSION['nombre'] = $nombre;
-    $_SESSION['tipoRol'] = $tipoRol;
-    $_SESSION['telefono'] = $telefono;
-    $_SESSION['direccion'] = $direccion;
-    $_SESSION['edad'] = $edad;
 	$_SESSION["autentic"] = true;
 
      
