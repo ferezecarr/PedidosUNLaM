250eb2e9801a6f57f6d811fd016ecd35cf17fec3
Duarte Lu <luisa.isabel.duarte@gmail.com>
2018-10-29 21:12:45 -0300

    cambiando la vista de la carga de menues

--- application/vistas/cargarMenu.php
+++ application/vistas/cargarMenu.php
@@ -1,6 +1,22 @@
 <?php
 
     require_once $_SERVER["DOCUMENT_ROOT"]. "/paths.php";
+    require_once $CONEXION_DIR;
+
+    $conexion= new Conexion();
+
+
+    $where = "";
+    
+    if(!empty($_POST))
+    {
+        $valor = $_POST['campo'];
+        if(!empty($valor)){
+            $where = "WHERE idMenu LIKE '%$valor'";
+        }
+    }
+    $sql = "SELECT * FROM menu $where";
+    $resultado = $conexion->query($sql);
 
 ?>
 
@@ -13,7 +29,8 @@
         require_once $HEADER_DIR;
 
     ?>
-
+        <script src="application/vistas/js/jquery-3.1.1.min.js"></script>
+        <script src="application/vistas/js/bootstrap.min.js"></script> 
 </head>
 <body>
 
@@ -32,38 +49,87 @@
                     <li class="breadcrumb-item active" aria-current="page"><a href="comercio.php">Comercio</a></li>
                     <li class="breadcrumb-item active" aria-current="page"><a href="cargarMenu.php">Cargar Menú</a></li>
                 </ol>
-            </nav>
+            </nav>    
+    
 
-            <div class="card" style="">
-                <div class="card-header">
-                    Cargar Menú
-                </div>
-                <div class="card-body" style="">
-                    <form action="<?php echo $ABM_MENU_HOST_ADD; ?>" method="post">
-                        <div class="form-group">
-                            <label for="titulo">Título:</label>
-                            <input type="text" name="titulo" id="titulo" class="form-control" placeholder="Escriba su titulo">
-                        </div>
-                        <div class="form-group">
-                            <label for="descripcion">Descripción:</label>
-                            <textarea class="form-control" name="descripcion" rows="5" id="descripcion" placeholder="Escriba su descripcion"></textarea>
-                        </div>
-                        <div class="form-group">
-                            <label for="precio">Precio:</label>
-                            <input type="number" class="form-control" name="precio" id="precio" placeholder="Escriba su precio">
-                        </div>
-                        <div class="form-group">
-                            <input type="file" class="form-control-file border">
-                        </div>
-                        <button type="submit" class="btn btn-success btn-lg btn-block">Guardar Menú</button>
-                        <button type="submit" class="btn btn-danger btn-lg btn-block">Cancelar</button>
-                    </form>
+
+    <div class="container-fluid">
+        <div class="card" style="">
+            
+            <div class="row">
+                <a href="<?php echo $ABM_MENU_HOST_ADD; ?>" class="btn btn-success">Nuevo Registro</a>
+                
+                <form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST">
+                    <b>IdMenu: </b><input type="text" id="campo" name="campo" />
+                    <input type="submit" id="enviar" name="enviar" value="Buscar" class="btn btn-success" />
+                </form>
+            </div>
+            
+            <br>
+            
+            <div class="row table-responsive">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Menu</th>
+                            <th>Precio</th>
+                            <th>Modificar</th>
+                            <th>Eliminar</th>
+                        </tr>
+                    </thead>
+                    
+                    <tbody>
+                        <?php while($row = $resultado->fetch_array(MYSQLI_ASSOC)) { ?>
+                            <tr>
+                                <td><?php echo $row['idMenu']; ?></td>
+                                <td><?php echo $row['titulo']; ?></td>
+                                <td><?php echo $row['precio']; ?></td>
+                                <td><button class="btn btn-success" >
+                                    <a href="modificar.php?id=<?php echo $row['idMenu']; ?>"></a>
+                                    </button>
+                                </td>
+                                <td>
+                                    <button class="btn btn-danger"
+                                    <a href="#" data-href="eliminar.php?id=<?php echo $row['idMenu']; ?>" data-toggle="modal" data-target="#confirm-delete"></a>
+                                </button>
+                                </td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        
+        <!-- Modal -->
+        <div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                        <h4 class="modal-title" id="myModalLabel">Eliminar Registro</h4>
+                    </div>
+                    
+                    <div class="modal-body">
+                        ¿Desea eliminar este registro?
+                    </div>
+                    
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                        <a class="btn btn-danger btn-ok">Delete</a>
+                    </div>
                 </div>
             </div>
         </div>
-        </div>
-    </div>
-    
-    
+     </div>
+        <script>
+            $('#confirm-delete').on('show.bs.modal', function(e) {
+                $(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
+                
+                $('.debug-url').html('Delete URL: <strong>' + $(this).find('.btn-ok').attr('href') + '</strong>');
+            });
+        </script>   
+        
 </body>
 </html>
